8df6c9e2837daa8f7ba1fb9581265baa6ecdb480
James Lucas <jamesmlucas@gmail.com>
2018-12-17 00:26:08 -0600

    headertext prop

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Platform, Text, View} from 'react-native';
 
-const Header = () => {
+const Header = (props) => {
   const {textStyle, viewStyle} = styles;
   return (
     <View style={viewStyle}>
-      <Text style={textStyle}>Albums fuck!</Text>
+      <Text style={textStyle}>{props.headerText}</Text>
     </View>
   );
 };
@@ -16,11 +16,18 @@ const styles = {
     justifyContent: "center", // vertical orientation
     alignItems: "center", // horizontal orientation
     height: 60,
-    paddingTop: 15,
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
-    elevation: 2 // required to get shadow working properly on android
+    position: "relative",
+    ...Platform.select({
+      ios: {
+        paddingTop: 15,
+        shadowColor: "#000",
+        shadowOffset: {width: 0, height: 2},
+        shadowOpacity: 0.2
+      },
+      android: {
+        elevation: 10 // required to get shadow working properly on android
+      }
+    }),
   },
   textStyle: {
     fontSize: 20
