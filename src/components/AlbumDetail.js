d0b8f1f2104a377a1f0b21fa2b6bbc860472c839
James Lucas <jamesmlucas@gmail.com>
2018-12-22 00:46:49 -0600

    more

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Image, Text, View} from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
@@ -7,10 +7,46 @@ const AlbumDetail = (props) => {
   return (
     <Card>
       <CardSection>
-      <Text>{props.album.title}</Text>
+        <View style={styles.thumbnailContainerStyle}>
+          <Image style={styles.thumbnailStyle}
+                 source={{uri: props.album.thumbnail_image}}/>
+        </View>
+        <View style={styles.headerContentStyle}>
+          <Text style={styles.headerTextStyle}>{props.album.title}</Text>
+          <Text>{props.album.artist}</Text>
+        </View>
+      </CardSection>
+      <CardSection>
+          <Image style={styles.albumImageStyle}
+                 source={{uri: props.album.image}}/>
       </CardSection>
     </Card>
   )
+};
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  albumImageStyle: {
+    height: 300,
+    flex: 1, // make it span entire width
+    width: null
+  }
 };
 
 export default AlbumDetail;
